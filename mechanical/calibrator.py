7829a60c16f7bc50663bfc9b6083e2672ddf5e65
Emily London <emilylondon@Emilys-MBP.lan>
2022-11-20 15:47:50 -0800

    Added remote control of the motors and the ability to send the coordinates and go to the correct position

---
+++ mechanical/calibrator.py
@@ -0,0 +1,39 @@
+from pynput.keyboard import Key, Listener
+import paho.mqtt.client as mqtt
+
+calibrated = False
+theta = 0
+phi = 0
+
+def on_press(key):
+    global calibrated
+    if key == Key.left:
+        client.publish("starryStarry/calibrate", "left")
+    elif key == Key.right:
+        client.publish("starryStarry/calibrate", "right")
+    elif key == Key.enter:
+        print("StarryStarryBot calibrated to North!")
+        calibrated = True
+        return False
+
+def on_release(key):
+    if key == Key.left or key == Key.right: 
+        client.publish("starryStarry/calibrate", "release")
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.loop_start()
+    with Listener(
+            on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+    while True: 
+        if calibrated == True:
+            coords = input("Enter coordinates: ")
+            client.publish("starryStarry/coordinates", coords)
---
+++ mechanical/movement.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import time
+import paho.mqtt.client as mqtt
+ 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+pwm=GPIO.PWM(11, 50)
+pwm.start(0)
+
+in1 = 17
+in2 = 18
+in3 = 27
+in4 = 22
+
+# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+step_sleep = 0.002
+ 
+step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
+ 
+direction = False # True for clockwise, False for counter-clockwise
+move = False #True for any movement, False for stopping 
+
+# defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+step_sequence = [[1,0,0,1],
+                 [1,0,0,0],
+                 [1,1,0,0],
+                 [0,1,0,0],
+                 [0,1,1,0],
+                 [0,0,1,0],
+                 [0,0,1,1],
+                 [0,0,0,1]]
+ 
+# setting up
+GPIO.setmode( GPIO.BCM )
+GPIO.setup( in1, GPIO.OUT )
+GPIO.setup( in2, GPIO.OUT )
+GPIO.setup( in3, GPIO.OUT )
+GPIO.setup( in4, GPIO.OUT )
+# initializing
+GPIO.output( in1, GPIO.LOW )
+GPIO.output( in2, GPIO.LOW )
+GPIO.output( in3, GPIO.LOW )
+GPIO.output( in4, GPIO.LOW )
+ 
+ 
+motor_pins = [in1,in2,in3,in4]
+motor_step_counter = 0 
+
+def move_steps(steps):
+    i = 0
+    global motor_step_counter 
+    for i in range(steps): 
+        for pin in range(0, len(motor_pins)):
+                GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+            if direction==True:
+                motor_step_counter = (motor_step_counter - 1) % 8
+            elif direction==False:
+                motor_step_counter = (motor_step_counter + 1) % 8
+            else: # defensive programming
+                print( "uh oh... direction should *always* be either True or False" )
+                cleanup()
+                exit( 1 )
+            time.sleep( step_sleep )
+
+def cleanup():
+    GPIO.output( in1, GPIO.LOW )
+    GPIO.output( in2, GPIO.LOW )
+    GPIO.output( in3, GPIO.LOW )
+    GPIO.output( in4, GPIO.LOW )
+    pwm.stop()
+    GPIO.cleanup()
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+    #subscribe to location topic
+    client.subscribe("starryStarry/calibrate")
+    client.subscribe("starryStarry/coordinates")
+    client.message_callback_add("starryStarry/calibrate", on_calibrate)
+    client.message_callback_add("starryStarry/coordinates", on_coordinate)
+
+def on_calibrate(client, userdata, message):
+    cal_str = message.payload.decode()
+    global move, direction, theta, motor_step_counter 
+    if cal_str == "left":
+        move = True 
+        direction = False 
+    elif cal_str == "right": 
+        move = True
+        direction = True 
+    elif cal_str == "release":
+        move = False 
+        theta = 0
+        motor_step_coutner = 0
+
+def on_coordinate(client, userdata, message):
+    global theta, phi, direction
+    coord_str = message.payload.decode().split('')
+    goal_theta = coord_str[0]
+    goal_phi = coord_str[1]
+    step_count = ((goal_theta-theta)/360)*4096
+
+    if step_count >= 0:
+        direction = True
+        move_steps(step_count)
+    else 
+        direction = False
+        move_steps(-step_count)
+
+    pwm.ChangeDutyCycle(goal_phi) 
+    theta = goal_theta
+    phi = goal_phi 
+
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.loop_start()
+    while True: 
+        if move:
+            move_steps(1)
+    
+
+ 
