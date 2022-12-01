a8e5b2d3e3f411bcce8f35a33d9f732da7eae194
CamSaidock <saidock@usc.edu>
2022-11-30 19:14:38 -0800

    Star

--- starry_app/src/App.js
+++ starry_app/src/App.js
@@ -4,24 +4,74 @@ function App() {
   return (
     <div className="App">
       <div className="Layout">
-        <div className='Top'>
-          <h2>Starry Starry Bot</h2>
-        </div>
-        <div className='Middle'>
-          
-        </div>
-        <div className='Bottom'>
-          <div className='Bottom-Left'>
-            <p>Los Angeles, CA</p>
-            <p>5:55</p>
-          </div>
-          <div className='Bottom-Right'>
-            <div className='Constellations'>
+        <div className="Left">
+          <h2>Star Map</h2>
+          <div className='Box'>
 
+          </div>
+        </div>
+        <div className="Right">
+          <div className='Right-Top'>
+            <h3>Location: <i>Los Angeles</i></h3>
+            <h3>Time: <i>6:40</i></h3>
+          </div>
+          <div className="List">
+            <div className='List-Option'>
+              <div className='List-Left'>
+                <h3>Star Name: Sirius</h3>
+              </div>
+              <div className='List-Right'>
+                <div className='Star-Image'/>
+              </div>
             </div>
-            <div className='Stars'>
-              
+
+            <div className='List-Option'>
+              <div className='List-Left'>
+                <h3>Star Name: Canopus</h3>
+              </div>
+              <div className='List-Right'>
+                <div className='Star-Image'/>
+              </div>
             </div>
+
+            <div className='List-Option'>
+              <div className='List-Left'>
+                <h3>Star Name: Rigil Kentaurus</h3>
+              </div>
+              <div className='List-Right'>
+                <div className='Star-Image'/>
+              </div>
+            </div>
+
+            <div className='List-Option'>
+              <div className='List-Left'>
+                <h3>Star Name: Arcturus</h3>
+              </div>
+              <div className='List-Right'>
+                <div className='Star-Image'/>
+              </div>
+            </div>
+
+            <div className='List-Option'>
+              <div className='List-Left'>
+                <h3>Star Name: Vega</h3>
+              </div>
+              <div className='List-Right'>
+                <div className='Star-Image'/>
+              </div>
+            </div>
+
+            <div className='List-Option'>
+              <div className='List-Left'>
+                <h3>Star Name: Capella</h3>
+              </div>
+              <div className='List-Right'>
+                <div className='Star-Image'/>
+              </div>
+            </div>
+          </div>
+          <div className='Button'>
+            <h3>Search for Star</h3>
           </div>
         </div>
       </div>
