4856cd47d2418a7a28b936f2f387525e0da48b0e
emilylondon <elondon@usc.edu>
2022-11-30 17:52:36 -0800

    Fixed servo stuff

--- mechanical/servo.py
+++ mechanical/servo.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.OUT)
 
-pwm=GPIO.PWM(11, 50)
+pwm=GPIO.PWM(13, 50)
 pwm.start(0)
 
 pwm.ChangeDutyCycle(5) # left -90 deg position
